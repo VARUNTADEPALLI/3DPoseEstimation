d31bf155ecdfc480994a4db994628e05a4e54129
VARUNTADEPALLI <40629984+VARUNTADEPALLI@users.noreply.github.com>
2024-05-11 13:32:38 -0400

    Update openvino.py

--- GTRS/GTRS-mains/openvino.py
+++ GTRS/GTRS-mains/openvino.py
@@ -201,10 +201,10 @@ TwoDPoseDetection.eval().to("cpu")
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-cpu = False if device == "cuda" else True
+cpu = True
+
 net = TwoDPoseDetection.eval()
-if not cpu:
-    net = net.cuda()
+# read image
 img = cv2.imread("/mnt/c/Users/varun/Downloads/3DHumanPoseEstimation/openpose/lightweight/input.jpeg", cv2.IMREAD_COLOR)
 stride = 8
 upsample_ratio = 4
@@ -236,40 +236,27 @@ stage2_pafs = stages_output[-1]
 pafs = np.transpose(stage2_pafs.squeeze().cpu().data.numpy(), (1, 2, 0))
 pafs = cv2.resize(pafs, (0, 0), fx=upsample_ratio, fy=upsample_ratio, interpolation=cv2.INTER_CUBIC)
 
-
 total_keypoints_num = 0
 all_keypoints_by_type = []
 for kpt_idx in range(num_keypoints):  # 19th for bg
-    total_keypoints_num += extract_keypoints(
-        heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num
-    )
+    total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
 
 pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs)
 for kpt_id in range(all_keypoints.shape[0]):
-    all_keypoints[kpt_id, 0] = (
-        all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]
-    ) / scale
-    all_keypoints[kpt_id, 1] = (
-        all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]
-    ) / scale
-all_poses = []
+    all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
+    all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
+current_poses = []
 for n in range(len(pose_entries)):
     if len(pose_entries[n]) == 0:
         continue
     pose_keypoints = np.ones((num_keypoints, 2), dtype=np.int32) * -1
     for kpt_id in range(num_keypoints):
-        if pose_entries[n][kpt_id] != -1.0:  # keypoint was found
-            pose_keypoints[kpt_id, 0] = int(
-                all_keypoints[int(pose_entries[n][kpt_id]), 0]
-            )
-            pose_keypoints[kpt_id, 1] = int(
-                all_keypoints[int(pose_entries[n][kpt_id]), 1]
-            )
-    all_poses.append(pose_keypoints)
+        if pose_entries[n][kpt_id] != -1.0:
+            pose_keypoints[kpt_id, 0] = int(all_keypoints[int(pose_entries[n][kpt_id]), 0])
+            pose_keypoints[kpt_id, 1] = int(all_keypoints[int(pose_entries[n][kpt_id]), 1])
+    current_poses.append(pose_keypoints)
 
-joint_input = all_poses[0]
-
-joint_img = joint_coordination(joint_input)
+joint_img = joint_coordination(current_poses[0])
 
 joint_img = torch.Tensor(joint_img)
 
